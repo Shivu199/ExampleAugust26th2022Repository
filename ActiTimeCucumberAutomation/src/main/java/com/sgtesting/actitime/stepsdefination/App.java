beaa45c66f51f6088a4d835e173c658796356bf5
Shivu199 <smk.jwl@gmail.com>
2022-11-23 07:52:16 +0530

    selenium folders

---
+++ ActiTimeCucumberAutomation/src/main/java/com/sgtesting/actitime/stepsdefination/App.java
@@ -0,0 +1,13 @@
+package com.sgtesting.actitime.stepsdefination;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- Web-Automation/src/com/sgtesting/ACTITIME_ASSINGMENTS/Assignment33.java
+++ Web-Automation/src/com/sgtesting/ACTITIME_ASSINGMENTS/Assignment33.java
@@ -1,4 +1,4 @@
-package com.Assignments.programs;
+package com.sgtesting.ACTITIME_ASSINGMENTS;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
